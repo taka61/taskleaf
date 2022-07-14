e9ef117c2a312c8d65aa9b4db9f2b36ce8b87d92
taka61 <taka.tennisloveup61@gmail.com>
2022-07-14 16:37:30 +0900

    usersコントローラに一覧・詳細・編集・削除機能を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,18 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -13,17 +25,25 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def edit
+  def updete
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
   end
 
-  def show
-  end
-
-  def index
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
-    def admin_users_path
-      params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
 end
