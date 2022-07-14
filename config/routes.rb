71b6f45c007d5008ac3b5491a334f7ad63528d95
taka61 <taka.tennisloveup61@gmail.com>
2022-07-14 14:01:15 +0900

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'users/new'
-    get 'users/edit'
-    get 'users/show'
-    get 'users/index'
+    resources :users
   end
+
   root to: 'tasks#index'
   resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
