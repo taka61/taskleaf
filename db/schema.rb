13ac4ed665474b874b77697edc052efb9a26405f
taka61 <taka.tennisloveup61@gmail.com>
2022-07-14 16:01:55 +0900

    shemaからusersテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,4 @@ ActiveRecord::Schema.define(version: 2022_07_14_054053) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
 end
