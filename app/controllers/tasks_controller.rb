0f4885cfc32087fb006d940c853eb2247c016d59
taka61 <taka.tennisloveup61@gmail.com>
2022-07-15 16:17:44 +0900

    重複箇所をset_taskメソッドを定義し切り出した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
@@ -18,32 +20,32 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
-
-    if task.update(task_params)
-      redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    if @task.update(task_params)
+      redirect_to @task, notice: "タスク「#{@task.name}」を更新しました。"
     else
       render :edit
     end
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
   
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
