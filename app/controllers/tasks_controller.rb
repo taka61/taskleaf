6be821271d3eae03c6bb9c3f75d5ad4acd7ffdcc
taka61 <taka.tennisloveup61@gmail.com>
2022-07-15 15:43:09 +0900

    タスク更新の際の条件を分岐

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,8 +27,12 @@ class TasksController < ApplicationController
 
   def update
     task = current_user.tasks.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+
+    if task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
